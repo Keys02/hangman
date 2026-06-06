1e73f9dd1ec1d27b982e8d8b9e1c7ca7cbbcde6c
Keys <keys14052002@gmail.com>
2026-06-06 00:56:38 +0000

    style: define all mistakes arts

--- src/Art.hpp
+++ src/Art.hpp
@@ -1,2 +1,81 @@
 #include <string_view>
+#include <array>
 
+namespace Hangman {
+    inline constexpr std::array<std::string_view, 7> arts = {
+    R"#(
+        +---+
+        |   |
+        |    
+        |    
+        |    
+        |
+        ========= 
+    )#",
+
+    R"#(
+        +---+
+        |   |
+        |   O
+        |    
+        |    
+        |
+        =========
+    )#",
+    R"#(
+        +---+
+        |   |
+        |   O
+        |   |
+        |    
+        |
+        =========
+    )#",
+
+    R"#(
+        +---+
+        |   |
+        |   O
+        |  /|
+        |    
+        |
+        =========
+    )#",
+
+    R"#(
+        +---+
+        |   |
+        |   O
+        |  /|\
+        |    
+        |
+        =========
+    )#",
+
+    R"#(
+        +---+
+        |   |
+        |   O
+        |  /|\
+        |  /
+        |
+        =========
+    )#",
+
+    R"#(
+        +---+
+        |   |
+        |   O
+        |  /|\
+        |  / \
+        |
+        =========
+    )#"
+    };
+
+    inline std::string_view getArts(int mistakes) {
+        if (mistakes < 0) mistakes = 0;
+        if (mistakes > 6)  mistakes = 6;
+        return arts[mistakes];
+    }
+}
