8947017dd2ea130874c2e6c4fda5769536f9434a
Keys <keys14052002@gmail.com>
2026-06-22 02:26:05 +0000

    chore: make keyboard state getter private

--- src/Player.hpp
+++ src/Player.hpp
@@ -12,9 +12,9 @@ namespace Hangman {
             std::set<char> getAllCorrectGuessedLetters() const;
             void updateKeyboardState(const char& input_char);
             void printKeyboard();
-            std::vector<std::string>& getKeyboardState();
         private:
             std::vector<std::string> keyboard {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+            std::vector<std::string>& getKeyboardState();
             std::size_t incorrectGuesses = 0;
             std::set<char> guessedCorrectLetters;
     };
