feaefe80051d235121e176de7d503d0e28c4f91a
Keys <keys14052002@gmail.com>
2026-06-22 02:17:00 +0000

    feat: display letters with already typed letters faded out

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,4 +1,7 @@
 #include "Player.hpp"
+#include <iostream>
+#include <cctype>
+#include <string>
 
 using namespace Hangman;
 
@@ -16,4 +19,30 @@ void Player::addCorrectLetterGuessed(char letter) {
 
 std::set<char> Player::getAllCorrectGuessedLetters() const {
     return guessedCorrectLetters;
+}
+
+void Player::updateKeyboardState(const char& input_char) {
+    for (std::string& keyboard_letter : getKeyboardState()) {
+
+        if (std::string (1, std::toupper(input_char)) == keyboard_letter) {
+            keyboard_letter = "\033[2m" + keyboard_letter + "\033[0m"; // fade out the key
+        }
+    }
+}
+
+void Player::printKeyboard() {
+    std::vector<std::string> keyboard_state = getKeyboardState();
+
+    for (size_t i = 0; i < keyboard_state.size(); ++i) {
+        if ((i + 1) % 13 == 0)
+            std::cout << keyboard_state[i] << '\n';
+        else
+            std::cout << keyboard_state[i] << ' ';
+    }
+
+    std::cout << "\n";
+}
+
+std::vector<std::string>& Player::getKeyboardState() {
+    return keyboard;
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstddef>
 #include <set>
+#include <string>
+#include <vector>
 namespace Hangman {
     class Player {
         public:
@@ -8,7 +10,11 @@ namespace Hangman {
             void incrIncorrectGuesses();
             void addCorrectLetterGuessed(char letter);
             std::set<char> getAllCorrectGuessedLetters() const;
+            void updateKeyboardState(const char& input_char);
+            void printKeyboard();
+            std::vector<std::string>& getKeyboardState();
         private:
+            std::vector<std::string> keyboard {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
             std::size_t incorrectGuesses = 0;
             std::set<char> guessedCorrectLetters;
     };
